9fde44223b5f0f1ba70fe34e31072dfed78f17ef
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-12 21:21:39 +0300

    Style Active Links Using usePathname Hook

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { CgSearchFound } from "react-icons/cg";
+import classNames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
   const links = [
     { href: "/", label: "Dashboard" },
     { href: "/issues", label: "Issues" },
@@ -15,7 +19,11 @@ const Navbar = () => {
       <ul className="flex space-x-4">
         {links.map((link) => (
           <Link
-            className="hover:text-zinc-600 transition-colors text-lg"
+            className={classNames({
+              "text-green-500": currentPath === link.href,
+              "text-black": currentPath !== link.href,
+              "hover:text-zinc-600 transition-colors text-lg font-bold": true,
+            })}
             key={link.href}
             href={link.href}
           >
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IssuesPage = () => {
+  return <div>Welcome To Issues Page</div>;
+};
+
+export default IssuesPage;

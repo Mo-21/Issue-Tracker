7c29f141eb851ef85025ee3732d1c1f142e977c7
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-14 02:10:21 +0300

    Add Delete Dialog Box

--- app/issues/[id]/DeleteButton.tsx
+++ app/issues/[id]/DeleteButton.tsx
@@ -1,14 +1,34 @@
 import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
 const DeleteButton = ({ issueId }: { issueId: string }) => {
   return (
-    <Button color="red">
-      <TrashIcon />
-      <Link href={`/issues/${issueId}/edit`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon />
+          <Link href={`/issues/${issueId}/edit`}>Delete Issue</Link>
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Are you sure?</AlertDialog.Title>
+        <AlertDialog.Description>
+          This will permanently delete the issue.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="2">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 

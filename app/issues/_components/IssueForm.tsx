ccd4072bad9e39436a5a1a6965d8b9643ed96c5e
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-13 23:12:25 +0300

    Fix Caching Issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -42,6 +42,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         await axios.post("/api/issues", data);
       }
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setError("Something went wrong");
       setIsSubmitting(false);

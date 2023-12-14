905559a4aff3de9281ad8dc88c61eb0857fa608a
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-15 01:54:28 +0300

    Add Login and Logout Links

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { CgSearchFound } from "react-icons/cg";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
   const currentPath = usePathname();
@@ -11,6 +12,9 @@ const Navbar = () => {
     { href: "/", label: "Dashboard" },
     { href: "/issues/list", label: "Issues" },
   ];
+
+  const { status, data: session } = useSession();
+
   return (
     <nav className="flex space-x-9 border-b px-5 h-14 items-center">
       <Link href="/">
@@ -18,19 +22,26 @@ const Navbar = () => {
       </Link>
       <ul className="flex space-x-4">
         {links.map((link) => (
-          <Link
-            className={classNames({
-              "text-green-500": currentPath === link.href,
-              "text-black": currentPath !== link.href,
-              "hover:text-zinc-600 transition-colors text-lg font-bold": true,
-            })}
-            key={link.href}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-green-500": currentPath === link.href,
+                "text-black": currentPath !== link.href,
+                "hover:text-zinc-600 transition-colors text-lg font-bold": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      {status === "authenticated" && (
+        <Link href="/api/auth/signout">Logout</Link>
+      )}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </nav>
   );
 };
---
+++ app/auth/Provide.tsx
@@ -0,0 +1,8 @@
+import React, { PropsWithChildren } from "react";
+import { SessionProvider } from "next-auth/react";
+
+const AuthProvider = ({ children }: PropsWithChildren) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthProvider;

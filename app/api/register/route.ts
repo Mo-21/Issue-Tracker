6ad88d92d6a5db1edf93d2a9e83503523a56b1ad
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-16 01:25:51 +0300

    Add Credentials Method For Registering Users

---
+++ app/api/register/route.ts
@@ -0,0 +1,35 @@
+import { registerSchema } from "@/app/validationSchemas";
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = registerSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const { email, password } = body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (user) {
+    return NextResponse.json("User already exists", { status: 400 });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json({ email: newUser.email });
+}
--- app/validationSchemas.tsx
+++ app/validationSchemas.tsx
@@ -1,11 +1,7 @@
 import { z } from "zod";
 
 export const patchIssueSchema = z.object({
-  title: z
-    .string()
-    .min(1, "Title is Required")
-    .max(255)
-    .optional(),
+  title: z.string().min(1, "Title is Required").max(255).optional(),
   description: z
     .string()
     .min(1, "Description is Required")
@@ -17,4 +13,24 @@ export const patchIssueSchema = z.object({
     .max(255)
     .optional()
     .nullable(),
+});
+
+export const registerSchema = z.object({
+  email: z.string().email("Invalid email").min(1, "Email is Required"),
+  password: z
+    .string()
+    .min(1, "Password is Required")
+    .min(8, "Password must be at least 8 characters long")
+    .includes(String(/[0-9]/), {
+      message: "Password must contain at least one number",
+    })
+    .includes(String(/[a-z]/), {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .includes(String(/[A-Z]/), {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .includes(String(/[^a-zA-Z\d]/), {
+      message: "Password must contain at least one special character",
+    }),
 });
